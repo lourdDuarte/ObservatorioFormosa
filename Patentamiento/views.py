eccb19d58b35a1d71e42fffef9fc82008009f906
unknown <lourdes123duarte@gmail.com>
2024-09-23 20:24:55 -0300

    changes

--- Patentamiento/views.py
+++ Patentamiento/views.py
@@ -6,19 +6,37 @@ def panel_auto(request):
     data_patentamiento = Indicadores.objects.select_related('mes','anio').values(
         'mes__mes',
         'anio__anio',
-        'movimiento_vehicular_id',
+        'movimiento_vehicular',
         'total',
         'total_acumulado'
     ).filter(tipo_vehiculo_id = 2)
-
-    if request.method == "POST":
-        año = request.POST.get('año')
-        
-        if año and año.isdigit():
-            patentamiento_filtro = 
-
+    print(data_patentamiento)
     context = {
         'data_patentamiento': data_patentamiento
     }
+    if request.method == "POST":
+        año = request.POST.get('año')
+       
+        
+        if año and año.isdigit():
+            patentamiento_filtro = Indicadores.objects.select_related('mes').values(
+                'mes__mes',
+                'movimiento_vehicular_id',
+                'variacion_interanual',
+                'variacion_intermensual'
+            ).filter(tipo_vehiculo_id = 2, movimiento_vehicular_id = 1, anio_id = año)
+
+            transferencia_filtro = Indicadores.objects.select_related('mes').values(
+                'mes__mes',
+                'movimiento_vehicular_id',
+                'variacion_interanual',
+                'variacion_intermensual'
+            ).filter(tipo_vehiculo_id = 2, movimiento_vehicular_id = 2, anio_id = año)
+            
+    
+            context.update({
+                'patentamiento_filtro': patentamiento_filtro,
+                'transferencia_filtro': transferencia_filtro
+            })
    
     return render (request, 'patentamiento/auto.html', context)
--- static/js/chart_auto/chart.js
+++ static/js/chart_auto/chart.js
@@ -1,3 +1,11 @@
 function patentamiento_auto(){
-    pass
+    let [interanual, intermensual, meses] = obtenerDatosPatentamiento();
+    chart = draw_line_chart( intermensual ,interanual,' ',meses,'chart-patentamiento-historico')
+
+}
+
+function transferencia_auto(){
+    let [interanual, intermensual, meses] = obtenerDatosTransferencia();
+    chart = draw_line_chart( intermensual ,interanual,' ',meses,'chart-transferencia-historico')
+
 }
